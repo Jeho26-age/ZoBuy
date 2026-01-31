a68b23e75fafd972809e9bf7dfbaf0fa8a322cc3
Jeho26-agw <shirodask777@gmail.com>
2026-02-01 01:37:06 +0530

    update

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-// --- ZoBuy MASTER CONFIGURATION ---
-// This file connects your GitHub site to Firebase and Cloudinary.
-
+// --- ZoBuy MASTER CONFIGURATION (Firestore Focus) ---
 const firebaseConfig = {
     apiKey: "AIzaSyBSKCAAu4BRhYwOmDoStr7WtkV0YCcQlS8",
     authDomain: "zobuy-95fdb.firebaseapp.com",
     projectId: "zobuy-95fdb",
     storageBucket: "zobuy-95fdb.firebasestorage.app",
     messagingSenderId: "480926307536",
-    appId: "1:480926307536:web:5489ed80f237aa49e43ad5",
-    databaseURL: "https://zobuy-95fdb-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    appId: "1:480926307536:web:5489ed80f237aa49e43ad5"
+    // databaseURL removed to avoid confusion with Firestore
 };
 
 const cloudinaryConfig = {
@@ -16,14 +14,12 @@ const cloudinaryConfig = {
     uploadPreset: "zobuy_preset"
 };
 
-// Initialize Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-// USE 'var' INSTEAD OF 'const' SO OTHER FILES CAN SEE THEM
+// Global variables for your other files to use
 var db = firebase.firestore();
-var rtdb = firebase.database();
 var auth = firebase.auth();
 
-console.log("ZoBuy Backend: Successfully Connected.");
+console.log("ZoBuy Backend: Firestore Connected.");
